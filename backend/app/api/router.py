6a9267ddae94beb991736ba3c03b0a66e51cba8b
Sebi <sebastian.torlakovic@gmail.com>
2026-07-28 06:32:18 +0200

    refactor: rename variables for clarity

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from api.routes.user import router as user_endpoint
+from api.routes.user import router as user_router
 from api.routes.auth import router as auth_router
-from api.routes.health import router as health_endpoint
+from api.routes.health import router as health_router
 
 api_router = APIRouter()
 
-api_router.include_router(health_endpoint)
-api_router.include_router(user_endpoint)
+api_router.include_router(health_router)
+api_router.include_router(user_router)
 api_router.include_router(auth_router)
