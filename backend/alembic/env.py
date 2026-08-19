fce5ebad8b3d2854eeeb06d35f83d3bb75546505
Sebastian <sebastian.torlakovic@gmail.com>
2026-08-19 18:26:49 +0000

    refactor: standardize app imports and Docker layout

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from app.db.base import Base
 from app.core.config import settings
-
+from app.models.user import User
 from alembic import context
 
 # this is the Alembic Config object, which provides
