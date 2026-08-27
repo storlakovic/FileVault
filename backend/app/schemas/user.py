789656d2a860d44b8b845ae1a6c062957569ea97
Sebi <sebastian.torlakovic@gmail.com>
2026-08-27 21:31:02 +0200

    feat(api): adjusted user-schema fields

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
 
 
 class UserCreate(BaseModel):
@@ -8,5 +8,8 @@ class UserCreate(BaseModel):
 
 
 class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
     username: str 
     email: EmailStr
+    is_active: bool
