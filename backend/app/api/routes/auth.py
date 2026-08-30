7598dd561b9798b76c550e7f9bbcb56a84c6d11e
Sebi <sebastian.torlakovic@gmail.com>
2026-08-30 14:26:09 +0200

    feat(auth): added userAlreadyExists error and handling

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 
 from app.api.dependencies.database import DatabaseSession
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
-from app.services.user_service import create_user
+from app.services.user_service import create_user, UserAlreadyExistsError
 
 router = APIRouter(
     prefix="/auth",
@@ -14,11 +14,20 @@ router = APIRouter(
              response_model=UserResponse,
              status_code=status.HTTP_201_CREATED,
 )
-def register(user_data: UserCreate, db: DatabaseSession) -> User:
-    return create_user(
-        db,
-        user_data,
-    )
+def register(
+    user_data: UserCreate,
+    db: DatabaseSession,
+) -> User:
+    try:
+        return create_user(
+            db,
+            user_data,
+        )
+    except UserAlreadyExistsError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Username or email already registered",
+        ) from exc
 
 @router.post("/login")
 def login():
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,14 +1,49 @@
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
+from sqlalchemy import or_, select
 
 from app.core.security import hash_password
 from app.models.user import User
 from app.schemas.user import UserCreate
 
 
+
+
+
+class UserAlreadyExistsError(Exception):
+    pass
+
+
 def create_user(db: Session, user_data: UserCreate) -> User:
+    statement = select(User).where(
+        or_(
+            User.email == user_data.email,
+            User.username == user_data.username,
+        )
+    )
+
+    existing_user = db.scalar(statement)
+
+    if existing_user is not None:
+        raise UserAlreadyExistsError()
+
     hashed_password = hash_password(user_data.password)
-    user = User(username=user_data.username, email=user_data.email, password_hash=hashed_password)
+
+    user = User(
+        username=user_data.username,
+        email=user_data.email,
+        password_hash=hashed_password
+    )
+
     db.add(user)
-    db.commit()
+
+    try:
+        db.commit()
+    except IntegrityError as exc:
+        db.rollback()
+        raise UserAlreadyExistsError()
+
     db.refresh(user)
+
     return user
+
